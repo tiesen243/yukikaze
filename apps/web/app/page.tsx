359a0aab153cc57334c4273136cab1af9982a576
tiesen243 <ttien56906@gmail.com>
2024-08-29 03:23:01 +0700

    feat: update db scripts and improve page layout
    - Updated the database scripts in package.json and turbo.json to include
      db:generate, db:push, and db:studio commands.
    - Improved the layout of the posts page by adding Tailwind CSS classes.
    - Removed default value for NODE_ENV in env.js to ensure proper environment
      configuration.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,17 +3,25 @@ import type { NextPage } from "next"
 import { api } from "@yuki/lib"
 
 const Page: NextPage = async () => {
-  const { data } = await api.post.getAll.get()
+  const { data } = (await api.post.getAll.get()) as {
+    data: {
+      posts: {
+        id: string
+        title: string
+        content: string
+      }[]
+    }
+  }
 
   return (
-    <div>
-      <h1>Posts</h1>
+    <div className="container my-4">
+      <h1 className="text-3xl font-bold">Posts</h1>
 
-      <ul>
+      <ul className="mt-4 grid grid-cols-3 gap-4">
         {data.posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
+          <li key={post.id} className="rounded-lg border p-6">
+            <h2 className="text-xl font-medium">{post.title}</h2>
+            <p className="text-sm">{post.content}</p>
           </li>
         ))}
       </ul>
